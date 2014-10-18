6fcb2bed351d93138db350dd35a30b090d3c0c29
Alexander Gabert <alexander.gabert@gmail.com>
2014-10-18 19:03:33 +0200

    bugfixes

--- puppet/modules/midolman/manifests/init.pp
+++ puppet/modules/midolman/manifests/init.pp
@@ -1,20 +1,20 @@
 #
 class midolman () inherits midolman::params {
 
-    configs = [
+    $configs = [
                 '/etc/midolman/midolman.conf',
                 '/etc/midolman/midolman-env.sh'
-              ]
+               ]
 
     package{ "midolman":
        ensure => "installed"
     }
     ->
-    midokura_puppet_types::types::t { configs: }
+    midokura_puppet_types::types::t { $configs: }
     ->
     service {"midolman":
       ensure => "started",
-      subscribe => configs
+      subscribe => $configs
     }
 
 }
