6ce32c02195f2ce153c25d5b6598a1a5eadd37c0
Alexander Gabert <alexander.gabert@gmail.com>
2014-10-26 06:24:39 +0100

    bugfix

--- puppet/modules/midonet_manager/manifests/params.pp
+++ puppet/modules/midonet_manager/manifests/params.pp
@@ -1,4 +1,8 @@
 class midonet_manager::params {
   $rest_api_base_url = 'http://localhost:8080/midonet-api/'
-  $root_url = '/midonet-cp2/'
+  if $::osfamily == "RedHat" {
+    $root_url = "/html/midonet-cp2/"
+  } else {
+    $root_url = "/midonet-cp2/"
+  }
 }
