aca0a5baefaa3eca57df218b7b3ee216d8feb586
Alexander Gabert <alexander.gabert@gmail.com>
2015-01-01 10:59:34 +0100

    add OSS variant to repo module

--- puppet/modules/midonet_repository/manifests/init.pp
+++ puppet/modules/midonet_repository/manifests/init.pp
@@ -31,6 +31,28 @@ class midonet_repository {
       unless => "/usr/bin/apt-key list | /bin/grep Midokura"
     }
     ->
+    file {"/etc/apt/sources.list.d/midokura.list":
+      ensure => "file",
+      path => "/etc/apt/sources.list.d/midokura.list",
+      content => template("midonet_repository/etc/apt/sources.list.d/midokura.list.erb"),
+      mode => "0644",
+      owner => "root",
+      group => "root",
+    }
+  }
+
+  define ubuntu_OSS($midonet_version = '2014.11',
+    $midonet_openstack_plugin_version = 'juno') {
+
+    package{ "curl":
+       ensure => "latest"
+    }
+    ->
+    exec {"${module_name}__install_package_key_on_osfamily_Debian":
+      command => "/usr/bin/curl -k http://repo.midonet.org/packages.midokura.key | /usr/bin/apt-key add -",
+      unless => "/usr/bin/apt-key list | /bin/grep Midokura"
+    }
+    ->
     file {"/etc/apt/sources.list.d/midonet.list":
       ensure => "file",
       path => "/etc/apt/sources.list.d/midonet.list",
