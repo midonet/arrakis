cec699db188a02e42ff88e24f353a57cd624572e
Alexander Gabert <alexander.gabert@gmail.com>
2015-01-08 15:11:39 +0100

    copyright notices updated

--- puppet/modules/midonet-neutron-plugin/manifests/midonet.pp
+++ puppet/modules/midonet-neutron-plugin/manifests/midonet.pp
@@ -1,7 +1,18 @@
-# MidoNet plugin for Neutron
 #
-# This configures the Neutron server to use the MidoNet plugin.
-
+# Copyright (c) 2015 Midokura SARL, All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 class neutron::plugins::midonet (
   $plugin_package             = 'python-neutron-plugin-midonet',
   $core_plugin                = 'midonet.neutron.plugin.MidonetPluginV2',
