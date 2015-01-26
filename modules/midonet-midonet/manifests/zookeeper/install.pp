fcc10c113dd3bb1740934bb592f355fd71d89a8c
Jaume Devesa <devvesa@gmail.com>
2015-01-26 16:27:46 +0000

    Add Cassandra service
    Some changes in ZooKeeper structure, too.
    
    Change-Id: Iee236e9e5876e4c47edaaea97089ec7f947448d7

--- modules/midonet-midonet/manifests/zookeeper/install.pp
+++ modules/midonet-midonet/manifests/zookeeper/install.pp
@@ -1,35 +1,63 @@
 # == Class: midonet::zookeeper::run
 # Check out the midonet::zookeeper class for a full understanding of
 # how to use the zookeeper resource
+#
+# === Authors
+#
+# Midonet (http://midonet.org)
+#
+# === Copyright
+#
+# Copyright (c) 2015 Midokura SARL, All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-class midonet::zookeeper::install {
+class midonet::zookeeper::install ($jre_package) {
 
     require midonet::repository
 
-    if $::osfamily == 'RedHat' {
-        package {"java-1.7.0-openjdk":
-            ensure => present
-        } ->
+    if ! defined(Package[$jre_package]) {
+        package {$jre_package:
+            ensure  => present,
+            require => Exec['update-repos']
+        }
+    }
 
+    if $::osfamily == 'RedHat' {
         file {['/usr/java', '/usr/java/default', '/usr/java/default/bin']:
-            ensure => directory
+            ensure  => directory,
+            require => Package[$jre_package]
         }
 
         file {'/usr/java/default/bin/java':
-            ensure => link,
-            target => '/usr/lib/jvm/jre-openjdk/bin/java'
+            ensure  => link,
+            target  => '/usr/lib/jvm/jre-openjdk/bin/java',
+            require => Package[$jre_package]
         }
 
         # For some reason, the zookeeper tries to be installed
         # before the repository in CentOS
         package {'zookeeper':
             ensure  => present,
-            require => Yumrepo["midonet-third-party"]
+            require => [Yumrepo['midonet-third-party'], Package[$jre_package]]
         }
     }
+
     else {
         package {'zookeeper':
-            ensure  => present
+            ensure  => present,
+            require => Package[$jre_package]
         }
     }
 
--- modules/midonet-midonet/manifests/zookeeper/run.pp
+++ modules/midonet-midonet/manifests/zookeeper/run.pp
@@ -1,29 +1,58 @@
 # == Class: midonet::zookeeper::run
 # Check out the midonet::zookeeper class for a full understanding of
 # how to use the zookeeper resource
+#
+# === Authors
+#
+# Midonet (http://midonet.org)
+#
+# === Copyright
+#
+# Copyright (c) 2015 Midokura SARL, All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-class midonet::zookeeper::run ($servers,
-                               $server_id,
-                               $zk_server_bin,
-                               $conf_file,
-                               $data_dir) {
+class midonet::zookeeper::run (
+  $servers,
+  $server_id,
+  $zk_server_bin,
+  $conf_file,
+  $conf_env_file,
+  $java_home,
+  $data_dir) {
 
     require midonet::zookeeper::install
 
-    file {"${data_dir}":
+    file {$data_dir:
         ensure => directory,
-        owner  => "zookeeper",
-        group  => "zookeeper"
+        owner  => 'zookeeper',
+        group  => 'zookeeper'
     } ->
 
     file {"${data_dir}/myid":
         content => $server_id
     } ->
 
-    file {"${conf_file}":
+    file {$conf_env_file:
+        ensure  => present,
+        content => template('midonet/zookeeper/zookeeper-env.sh.erb')
+    } ->
+
+    file {$conf_file:
         ensure  => present,
         content => template('midonet/zookeeper/zoo.cfg.erb')
-    }
+    } ~>
 
     # Using the binary provided by the zookeeper package instead of the upstart
     # daemon, this way we can easily deploy the puppet module into a container.
@@ -36,7 +65,6 @@ class midonet::zookeeper::run ($servers,
         hasstatus  => true,
         hasrestart => true,
         enable     => true,
-        provider   => 'base',
-        require    => File["${conf_file}"]
+        provider   => 'base'
     }
 }
