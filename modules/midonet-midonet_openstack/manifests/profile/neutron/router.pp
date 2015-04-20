b14fa58ee0210bc39f238def6d143afca1777401
Jaume Devesa <devvesa@gmail.com>
2015-04-20 18:48:28 +0200

    Improvements on multinode
    Change-Id: I6202c8159a9bcfa638673cdb6f87db88fe2e45c5

--- modules/midonet-midonet_openstack/manifests/profile/neutron/router.pp
+++ modules/midonet-midonet_openstack/manifests/profile/neutron/router.pp
@@ -1,21 +1,21 @@
 class midonet_openstack::profile::neutron::router {
 
-  $controller_management_address = $::openstack_midonet::config::controller_address_management
+  $controller_management_address = $::openstack::config::controller_address_management
 
   include ::openstack::common::neutron
   include ::midonet::midonet_agent
 
   class { '::neutron::agents::dhcp':
-    debug   => $::openstack_midonet::config::debug,
+    debug   => $::openstack::config::debug,
     enabled => true,
   }
 
   class { '::neutron::agents::metadata':
-    auth_password => $::openstack_midonet::config::neutron_password,
-    shared_secret => $::openstack_midonet::config::neutron_shared_secret,
+    auth_password => $::openstack::config::neutron_password,
+    shared_secret => $::openstack::config::neutron_shared_secret,
     auth_url      => "http://${controller_management_address}:35357/v2.0",
-    debug         => $::openstack_midonet::config::debug,
-    auth_region   => $::openstack_midonet::config::region,
+    debug         => $::openstack::config::debug,
+    auth_region   => $::openstack::config::region,
     metadata_ip   => $controller_management_address,
     enabled       => true,
   }
