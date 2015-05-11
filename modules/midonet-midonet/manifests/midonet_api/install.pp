ca429c655469c63880105cf0262ff8e3a5a07e16
Jaume Devesa <devvesa@gmail.com>
2015-05-11 10:03:36 +0200

    Add port on tomcat manifest
    Let the user choose the port where to deploy the tomcat
    
    Change-Id: Ic20158ce4003ecb63e71e9fe40c4995ca712205a

--- modules/midonet-midonet/manifests/midonet_api/install.pp
+++ modules/midonet-midonet/manifests/midonet_api/install.pp
@@ -22,7 +22,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-class midonet::midonet_api::install($tomcat_package) {
+class midonet::midonet_api::install(
+  $tomcat_package,
+  $tomcat_port) {
     require midonet::repository
 
     if ! defined(Class['java']) {
